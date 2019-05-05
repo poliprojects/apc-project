2719a3f043b9230d491bcfcd0b760f02c4bb16de
MicheleVidulis <michele.vidulis@outlook.com>
2019-05-05 19:20:00 +0000

    Riorganizzato e commentato il main, aggiunto README con istruzioni

--- main.cpp
+++ main.cpp
@@ -6,32 +6,35 @@
 
 int main()
 {
+  //Initialization of test specific features
   #if defined TEST_1
     std::cout << "Running Test 1" << '\n';
     double final_time = 1;
-    EquationFunction fun;
     Rnvector initial_condition{0};
-    BaseEquation equation(final_time, fun, initial_condition);
     double initial_step = 0.01;
-    FESolver problem(initial_step, equation);
-
-    problem.solve();
-    problem.print();
-    problem.save_sol_to_file("solution_1.txt");
-
   #elif defined TEST_2
     std::cout << "Running Test 2" << '\n';
     double final_time = 10;
-    EquationFunction fun;
     Rnvector initial_condition{0};
-    BaseEquation equation(final_time, fun, initial_condition);
     double initial_step = 0.1;
-    FESolver problem(initial_step, equation);
+  #endif
 
-    problem.solve();
-    problem.print();
+  //Initialization of the solver
+  //NB: the actual definition of fun depends on the test chosen (see utils.hpp)
+  EquationFunction fun;
+  BaseEquation equation(final_time, fun, initial_condition);
+  FESolver problem(initial_step, equation);
+
+  //Solves the problem
+  problem.solve();
+
+  //Prints problem characteristics on screen
+  problem.print();
+
+  #if defined TEST_1
+    problem.save_sol_to_file("solution_1.txt");
+  #elif defined TEST_2
     problem.save_sol_to_file("solution_2.txt");
-
   #endif
 
   return 0;
