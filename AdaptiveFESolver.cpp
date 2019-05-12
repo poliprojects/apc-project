d622cc26c8dfde055d197643af8b811ffe396f8f
Bruno Guindani <bruno@guindani.org>
2019-05-12 21:51:13 +0200

    Fixed AdapFE again

--- AdaptiveFESolver.cpp
+++ AdaptiveFESolver.cpp
@@ -1,76 +1,52 @@
 #include "AdaptiveFESolver.hpp"
 
 
-double AdaptiveFESolver::step(const double tbar, const Rnvector &ubar,
-	const Rnvector &fubar) const
-{
-
-	double hn = h; // initialize adaptive step to starting step
-	bool stop = false;
-
-	// Adaptive loop
-	while(hn / 2 >= hmin and !stop)
-	{
-		// Compute two solutions with different steps through FE method
-		Rnvector uh = ubar + hn * fubar;
-		Rnvector uh2 = ubar + (hn / 2) * fubar;
-
-		// Compute error in infinity norm
-    Rnvector diff = abs(uh - uh2);
-		double error = *std::max_element( diff.cbegin(), diff.cend() );
-
-		if( error < tol/2 ) // termination criterion
-    {
-      stop = true;
-      //std::cout << "tol\n"; //DEBUG
-    }
-    else
-    {
-      hn = hn/2;
-      //std::cout << "half\n"; //DEBUG
-    }
-	}
-
-	return hn/2;
-}
-
-
 void AdaptiveFESolver::solve()
 {
   // Initialization of the time instants vector
   times.push_back( equation.get_tin() );
   double tfin = equation.get_tfin();
 
-  // Take solution at time 0 and function f from data
-  Rnvector un = solution[0];
+  Rnvector un = solution[0]; // solution at n-th time, initialized at t=tin
   EquationFunction f = equation.get_f();
-
-  // Solution loop
-  Rnvector un1( un.size() ); // solution at time n+1
+  
   unsigned n = 0;
+  double hn = h;
+  double tn = times[0];
   bool stop = false;
 
-  while( !stop )
+  // Solution loop
+  while( tn < tfin )
   {
-    Rnvector f_eval = f( times[n], un );
-    double hn = step(times[n], un, f_eval); // compute adaptive step
+    // Single iteration with step hn
+    Rnvector uh1 = un + hn*f(tn, un);
 
-    if( times[n] + hn > tfin ) // out of range
-      stop = true;
+    // Double iteration with step hn/2
+    Rnvector utemp = un + (hn/2)*f(tn, un);
+    Rnvector uh2   = utemp + (hn/2)*f(tn + hn/2, utemp);
+
+    // Compute error in infinity norm
+    Rnvector diff = abs( uh2 - uh1 );
+    double error = *std::max_element( diff.cbegin(), diff.cend() );
+
+    if( error < tol/2 or hn < hmin ) // termination criteria
+    {
+      times.push_back( tn + hn );
+      tn += hn;
+      solution.push_back( uh2 );
+      un = uh2;
+
+      hn *= 2;
+      n++;
+    }
     else
     {
-      times.push_back( times[n] + hn );
-      un1 = un + hn*f_eval;
-      solution.push_back( un1 );
-
-      un = un1;
-      un1.clear();
-      n++;
+      hn = hn/2;
     }
 
   }
 
-  //for(auto &ti:times) std::cout << ti << " "; //DEBUG
+  for(auto &ti:times) std::cout << ti << " "; //DEBUG
 }
 
 void AdaptiveFESolver::print_solver_spec() const
