568b35e562133624add5e9fc585b0d586975052e
Bruno Guindani <bruno@guindani.org>
2019-06-07 13:32:26 +0200

    Reordering of RK4S

--- RK4Solver.hpp
+++ RK4Solver.hpp
@@ -11,13 +11,15 @@ class RK4Solver: public RKSolver
 	RK4Solver(double step, const BaseEquation &eq):
 		RKSolver(
 			step, eq,
+			// matrix a
 			{	{.0, .5, .0, .0},
 				{.0, .0, .5, .0},
 				{.0, .0, .0, 1.},
-				{.0, .0, .0, .0}
-			},
-		{.0, .5, .5, 1.},
-		{1/.6, 1/.3, 1/.3, 1/.6}
+				{.0, .0, .0, .0} },
+			// matrix b
+			{.0, .5, .5, 1.},
+			// matrix c
+			{1/.6, 1/.3, 1/.3, 1/.6}
 		) {}
 
 	void print_solver_spec() const override;
