cd580853c59634fe1257e40dbdd37cb7037362e2
MicheleVidulis <michele.vidulis@outlook.com>
2019-06-08 20:41:17 +0000

    Removed RK4Solver

--- RK4Solver.cpp
+++
@@ -1,9 +0,0 @@
-#include "RK4Solver.hpp"
-
-void RK4Solver::print_solver_spec() const
-{
-	std::cout << "Solved using: RK4" << std::endl;
-	std::cout << "h  = " << h << std::endl;
-	std::cout << "Nh = " << Nh << std::endl;
-	std::cout << std::endl;
-}
