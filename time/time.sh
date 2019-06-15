b1bbad64d66635880ac416cd6ef32a0b690417e5
Bruno Guindani <bruno@guindani.org>
2019-06-16 00:48:39 +0200

    Merged script into the single time.sh and renamed my own output files

---
+++ time/time.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# RUN THIS SCRIPT WHILE IN time FOLDER!!!!!
+
+# Remove files, if any, of a previous run of this script (the ones not starting
+# with "time" will not be deleted)
+rm -rf time*.txt
+
+# Set number of iterations
+n=100
+
+# Execute all programs and write execution times into the corresponding files:
+# * grep 'μ' extracts from the output the line with the duration time
+# * grep -oP "\d{4,}" further extracts a sequence of 4 or more digits
+# * >> <file>.txt appends the found number to the corresponding file
+for (( i=1; i<=$n; i++ )); do
+    # Test number 5, parallel
+    mpiexec -np 2 ../main 5 IserNor | grep 'μ' | grep -oP "\d{4,}" >> \
+        timepar5.txt
+    # Test number 5, sequential
+    ../main 5 IserNor | grep 'μ' | grep -oP "\d{4,}" >> timeseq5.txt
+
+    # Test number 6, parallel
+    mpiexec -np 2 ../main 6 IserNor | grep 'μ' | grep -oP "\d{4,}" >> \
+        timepar6.txt
+    # Test number 6, sequential
+    ../main 6 IserNor | grep 'μ' | grep -oP "\d{4,}" >> timeseq6.txt
+
+    echo "Round $i of $n"
+done
+
+# Remove useless solution files
+rm sol*.txt
+
+
+# Compute sum of times for each file and write output into timeres.txt
+echo "Parallel 5:" >> timeres.txt
+awk '{ sum += $1 } END { print sum }' timepar5.txt >> timeres.txt
+echo >> timeres.txt
+
+echo "Sequent. 5:" >> timeres.txt
+awk '{ sum += $1 } END { print sum }' timeseq5.txt >> timeres.txt
+echo >> timeres.txt
+
+echo >> timeres.txt
+
+echo "Parallel 6:" >> timeres.txt
+awk '{ sum += $1 } END { print sum }' timepar6.txt >> timeres.txt
+echo >> timeres.txt
+
+echo "Sequent. 6:" >> timeres.txt
+awk '{ sum += $1 } END { print sum }' timeseq6.txt >> timeres.txt
+
+echo "Sums were saved into the timeres.txt file"
