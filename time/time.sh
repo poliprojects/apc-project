da91cf7035db46efb9b3afcdadf22be0dfb08839
Bruno Guindani <bruno@guindani.org>
2019-07-02 17:57:41 +0200

    Added test 4 to time.sh

--- time/time.sh
+++ time/time.sh
@@ -14,6 +14,12 @@ n=100
 # * grep -oP "\d{4,}" further extracts a sequence of 4 or more digits
 # * >> <file>.txt appends the found number to the corresponding file
 for (( i=1; i<=$n; i++ )); do
+    # Test number 4, parallel
+    mpiexec -np 2 ../main 4 IserNor | grep 'μ' | grep -oP "\d{4,}" >> \
+        timepar4.txt
+    # Test number 4, sequential
+    ../main 4 IserNor | grep 'μ' | grep -oP "\d{4,}" >> timeseq4.txt
+
     # Test number 5, parallel
     mpiexec -np 2 ../main 5 IserNor | grep 'μ' | grep -oP "\d{4,}" >> \
         timepar5.txt
@@ -34,6 +40,14 @@ rm sol*.txt
 
 
 # Compute sum of times for each file and write output into timeres.txt
+echo "Parallel 4:" >> timeres.txt
+awk '{ sum += $1 } END { print sum }' timepar4.txt >> timeres.txt
+echo >> timeres.txt
+
+echo "Sequent. 4:" >> timeres.txt
+awk '{ sum += $1 } END { print sum }' timeseq4.txt >> timeres.txt
+echo >> timeres.txt
+
 echo "Parallel 5:" >> timeres.txt
 awk '{ sum += $1 } END { print sum }' timepar5.txt >> timeres.txt
 echo >> timeres.txt
